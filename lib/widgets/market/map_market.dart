c93dc9fcf5d8575e9f78b9fab6805bb1087f1fa1
wmfadel <mohamed.fadel24@hotmail.com>
2021-04-09 11:42:39 +0200

    changed launcher icon

--- lib/widgets/market/map_market.dart
+++ lib/widgets/market/map_market.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:shelf/models/market_book.dart';
 import 'package:shelf/providers/auth_provider.dart';
 import 'package:shelf/providers/map_provider.dart';
 import 'package:shelf/providers/market_provider.dart';
