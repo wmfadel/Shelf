509e68cc4e0b49e9480fef2461c1b23cdaf2b0e7
wmfadel <mohamed.fadel24@hotmail.com>
2021-04-09 15:51:22 +0200

    completed my market page

--- lib/pages/my_market_page.dart
+++ lib/pages/my_market_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shelf/providers/auth_provider.dart';
+import 'package:shelf/widgets/market/on_market.dart';
 
 class MyMarkeyPage extends StatelessWidget {
   static final String routeName = 'MyMarkey_page';
@@ -7,6 +10,9 @@ class MyMarkeyPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('My market'),
+      ),
+      body: OnMarket(
+        userID: Provider.of<AuthProvider>(context, listen: false).uid,
       ),
     );
   }
