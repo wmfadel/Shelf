ce369ed67c1c5c51de3b65c1607048dffe01bf8e
wmfadel <mohamed.fadel24@hotmail.com>
2021-06-01 22:33:50 +0200

    fixed message text sizing

--- lib/widgets/chat/message_item.dart
+++ lib/widgets/chat/message_item.dart
@@ -13,48 +13,41 @@ class MessageItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     authProvider = Provider.of<AuthProvider>(context, listen: false);
-    bool shouldRevere = isFromThisUser();
-
-    List<Widget> items = [
-      CircleAvatar(
-        backgroundImage: NetworkImage(
-          isFromThisUser() ? authProvider.photo! : otherUser.photo,
-        ),
-      ),
-      Container(
-        child: Column(
-          children: [
-            Text(
-              message.text!,
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
-        margin: EdgeInsets.only(
-          left: isFromThisUser() ? 20 : 5,
-          right: isFromThisUser() ? 5 : 20,
-        ),
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: isFromThisUser()
-              ? Theme.of(context).primaryColor.withOpacity(0.75)
-              : Colors.grey.withOpacity(0.7),
-          borderRadius: BorderRadius.only(
-            topLeft: isFromThisUser() ? Radius.circular(8) : Radius.zero,
-            topRight: isFromThisUser() ? Radius.zero : Radius.circular(8),
-            bottomLeft: isFromThisUser() ? Radius.circular(8) : Radius.zero,
-            bottomRight: isFromThisUser() ? Radius.zero : Radius.circular(8),
-          ),
-        ),
-      ),
-    ];
 
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 2),
       child: Row(
+        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment:
             isFromThisUser() ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: shouldRevere ? items.reversed.toList() : items,
+        children: [
+          Flexible(
+            child: Container(
+              child: Text(
+                message.text!,
+                style: TextStyle(color: Colors.white),
+              ),
+              margin: EdgeInsets.only(
+                left: isFromThisUser() ? 50 : 5,
+                right: isFromThisUser() ? 5 : 50,
+              ),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: isFromThisUser()
+                    ? Theme.of(context).primaryColor.withOpacity(0.75)
+                    : Colors.grey[300],
+                borderRadius: BorderRadius.only(
+                  topLeft: isFromThisUser() ? Radius.circular(8) : Radius.zero,
+                  topRight: isFromThisUser() ? Radius.zero : Radius.circular(8),
+                  bottomLeft:
+                      isFromThisUser() ? Radius.circular(8) : Radius.zero,
+                  bottomRight:
+                      isFromThisUser() ? Radius.zero : Radius.circular(8),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
